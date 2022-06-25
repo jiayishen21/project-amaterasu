aad176ea9742628fc1bf27bf866c39e2feedc543
Lucas Shen <lucasshen21@gmail.com>
2022-06-25 03:13:04 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import background from './assets/background.png';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
